593096be91ba0a29b07830a93af9ac9ad93d74f1
Min Naing Oo <minnaingoo792@gmail.com>
2024-06-09 09:14:56 +0630

    java&query-done

---
+++ Git-lesson/html/javascript.js
@@ -0,0 +1,13 @@
+let bars = document.querySelector("#bars")
+console.log(bars)
+let navLinks = document.querySelector(".nav-links")
+console.log(navLinks)
+
+bars .addEventListener("click", ()=>
+    {
+        navLinks.classList.toggle("show")
+        bars.classList.toggle("fa-times")
+    }
+
+
+)
