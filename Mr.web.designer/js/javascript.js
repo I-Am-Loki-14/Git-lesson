3200f1aabca8ca751b5612b48a6f17873cdbea8d
Min Naing Oo <minnaingoo792@gmail.com>
2024-06-20 10:43:20 +0630

    webdesigner

---
+++ Mr.web.designer/js/javascript.js
@@ -0,0 +1,10 @@
+// let bars = document.querySelector('.bars');
+// console.log
+// let navlinks =document.querySelector('.navlinks');
+// console.log
+
+// bars.onclick = () =>{
+//     navlinks.classList.toggle('active');
+//     bars.classList.toggle('fa-times');
+
+// }
